1968129f5afe4c3c77755418cc8f29a54af9da83
Max Yuan <maxyuan6717@gmail.com>
2021-01-26 22:37:57 -0500

    Restructure some code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Row, Col } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { SET_VAL } from "./redux/masterReducer";
 import axios from "axios";
 import { casCheck } from "./util/api";
 import Landing from "./pages/Landing";
 import Write from "./pages/Write";
+import styles from "./App.module.css";
+import heart from "./assets/pixel_heart.png";
+import logo from "./assets/wybc_logo.png";
 
 function App() {
   const dispatch = useDispatch();
@@ -32,12 +36,46 @@ function App() {
   }, [dispatch]);
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/write" component={Write} />
-        <Route path="/" component={Landing} />
-      </Switch>
-    </Router>
+    <Row className={styles.container + " mx-auto"}>
+      <Col xs={1} className={`${styles.title} ${styles.crush} ml-1 p-0`}>
+        <Row className={`mx-auto justify-content-center mt-4 ${styles.logo}`}>
+          <a href="https://wybc.com/">
+            <img
+              src={logo}
+              width={32}
+              className="mx-auto"
+              style={{ display: "block" }}
+              alt="logo"
+            />
+          </a>
+        </Row>
+        <Row className="mx-auto justify-content-center">C</Row>
+        <Row className="mx-auto justify-content-center">R</Row>
+        <Row className="mx-auto justify-content-center">U</Row>
+        <Row className="mx-auto justify-content-center">S</Row>
+        <Row className="mx-auto justify-content-center">H</Row>
+        <Row
+          className="mx-auto justify-content-center"
+          style={{ marginTop: "15px" }}
+        >
+          <img
+            src={heart}
+            width={35}
+            height={35}
+            className="mx-auto"
+            alt="heart"
+          />
+        </Row>
+      </Col>
+      <Col xs={10} className="px-2">
+        <Router>
+          <Switch>
+            <Route exact path="/write" component={Write} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </Router>
+      </Col>
+    </Row>
   );
 }
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col, Fade } from "react-bootstrap";
+import { Row, Fade } from "react-bootstrap";
 import styles from "./Landing.module.css";
-import heart from "../assets/pixel_heart.png";
-import logo from "../assets/wybc_logo.png";
 import { fetchStudents } from "../util/api";
 import { SET_VAL } from "../redux/masterReducer";
 import { useSelector, useDispatch } from "react-redux";
@@ -49,105 +47,71 @@ const Landing = () => {
   };
 
   return (
-    <Row className={styles.container + " mx-auto"}>
-      <Col xs={1} className={`${styles.title} ${styles.crush} ml-1 p-0`}>
-        <Row className={`mx-auto justify-content-center mt-4 ${styles.logo}`}>
-          <a href="https://wybc.com/">
-            <img
-              src={logo}
-              width={32}
-              className="mx-auto"
-              style={{ display: "block" }}
-              alt="logo"
-            />
-          </a>
+    <Row className="mx-auto" style={{ height: "100%" }}>
+      <div
+        className="m-auto py-3"
+        style={{ width: "500px", maxWidth: "500px" }}
+      >
+        <Row className={`mx-auto ${styles.header}`}>HEADER TEXT</Row>
+        <Row className={`mx-auto my-2 ${styles.description}`}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum
+          lorem sed risus ultricies tristique nulla aliquet enim tortor. In
+          pellentesque massa placerat duis ultricies lacus sed. Augue interdum
+          velit euismod in pellentesque. Volutpat odio facilisis mauris sit amet
+          massa vitae tortor condimentum.
         </Row>
-        <Row className="mx-auto justify-content-center">C</Row>
-        <Row className="mx-auto justify-content-center">R</Row>
-        <Row className="mx-auto justify-content-center">U</Row>
-        <Row className="mx-auto justify-content-center">S</Row>
-        <Row className="mx-auto justify-content-center">H</Row>
-        <Row
-          className="mx-auto justify-content-center"
-          style={{ marginTop: "15px" }}
-        >
-          <img
-            src={heart}
-            width={35}
-            height={35}
-            className="mx-auto"
-            alt="heart"
-          />
-        </Row>
-      </Col>
-      <Col xs={10} className="px-2">
-        <Row className="mx-auto" style={{ height: "100%" }}>
-          <div
-            className="m-auto py-3"
-            style={{ width: "500px", maxWidth: "500px" }}
-          >
-            <Row className={`mx-auto ${styles.header}`}>HEADER TEXT</Row>
-            <Row className={`mx-auto my-2 ${styles.description}`}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Vestibulum lorem sed risus ultricies tristique nulla aliquet enim
-              tortor. In pellentesque massa placerat duis ultricies lacus sed.
-              Augue interdum velit euismod in pellentesque. Volutpat odio
-              facilisis mauris sit amet massa vitae tortor condimentum.
-            </Row>
 
-            {!loadingState ? (
-              "loading..."
-            ) : stateVal.author_netId ? (
-              <>
-                <Row className="mx-auto mb-3">
-                  <div style={{ width: "100%" }}>
-                    <AsyncSelect
-                      loadOptions={loadOptions}
-                      placeholder="Type in a recipient's name..."
-                      autoFocus
-                      onChange={onInputChange}
-                      isClearable={true}
-                      value={
-                        stateVal.recipient_email
-                          ? {
-                              value: stateVal.recipient_email,
-                              label: stateVal.recipient_name,
-                            }
-                          : null
-                      }
-                      styles={select_styles}
-                    />
-                  </div>
-                </Row>
-                <Fade in={stateVal.recipient_email}>
-                  <Row
-                    className="mx-auto"
-                    style={{
-                      pointerEvents: stateVal.recipient_email ? null : "none",
-                    }}
-                  >
-                    <Link to="/write" style={{ width: "100%" }}>
-                      <StyledBtn className="d-flex justify-content-center">
-                        Invite →
-                      </StyledBtn>
-                    </Link>
-                  </Row>
-                </Fade>
-              </>
-            ) : (
-              <StyledBtn
-                className="mt-3 d-flex justify-content-center"
-                onClick={() => {
-                  window.location.href = `${Base}/auth/cas`;
+        {!loadingState ? (
+          "loading..."
+        ) : stateVal.author_netId ? (
+          <>
+            <Row className="mx-auto mb-3">
+              <div style={{ width: "100%" }}>
+                <AsyncSelect
+                  loadOptions={loadOptions}
+                  placeholder="Type in a recipient's name..."
+                  autoFocus
+                  onChange={onInputChange}
+                  isClearable={true}
+                  value={
+                    stateVal.recipient_email
+                      ? {
+                          value: stateVal.recipient_email,
+                          label: stateVal.recipient_name,
+                        }
+                      : null
+                  }
+                  styles={select_styles}
+                />
+              </div>
+            </Row>
+            <Fade in={stateVal.recipient_email}>
+              <Row
+                className="mx-auto"
+                style={{
+                  pointerEvents: stateVal.recipient_email ? null : "none",
                 }}
               >
-                Login with CAS
-              </StyledBtn>
-            )}
-          </div>
-        </Row>
-      </Col>
+                <Link to="/write" style={{ width: "100%" }}>
+                  <StyledBtn className="d-flex justify-content-center">
+                    Invite →
+                  </StyledBtn>
+                </Link>
+              </Row>
+            </Fade>
+          </>
+        ) : (
+          <StyledBtn
+            className="mt-3 d-flex justify-content-center"
+            onClick={() => {
+              window.location.href = `${Base}/auth/cas`;
+            }}
+          >
+            Login with CAS
+          </StyledBtn>
+        )}
+      </div>
     </Row>
   );
 };
