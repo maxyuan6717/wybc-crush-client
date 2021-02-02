c49db0409583bad6c257612f5c11dfb89429cac8
Max Yuan <maxyuan6717@gmail.com>
2021-02-02 00:27:09 -0500

    put branding on top for mobile

--- src/App.js
+++ src/App.js
@@ -38,58 +38,93 @@ function App() {
   }, [dispatch]);
 
   return (
-    <Row className={styles.container + " mx-auto"}>
-      <Col xs={1} className="p-0 d-block d-md-none" />
-      <Col
-        xs={1}
-        className={`${styles.title} ${styles.crush} ml-1 p-0 d-none d-md-block`}
+    <>
+      <div
+        className="mx-auto mb-2 d-block d-md-none justify-content-center"
+        style={{ width: "100vw" }}
       >
-        <Row className={`mx-auto justify-content-center mt-4 ${styles.logo}`}>
-          <a href="https://wybc.com/">
-            <img
-              src={logo}
-              width={32}
-              className="mx-auto"
-              style={{ display: "block" }}
-              alt="logo"
-            />
-          </a>
-        </Row>
-        <Col className={styles.crush_btn + " px-0"}>
-          <a href="https://wybc-crush.live" style={{ color: "inherit" }}>
-            <Row className="mx-auto justify-content-center">C</Row>
-            <Row className="mx-auto justify-content-center">R</Row>
-            <Row className="mx-auto justify-content-center">U</Row>
-            <Row className="mx-auto justify-content-center">S</Row>
-            <Row className="mx-auto justify-content-center">H</Row>
-
-            <Row
-              className="mx-auto pb-3 justify-content-center"
-              style={{ marginTop: "15px" }}
-            >
+        <Row className="mx-auto">
+          <Col className="p-0" xs={1} />
+          <Col xs={10} className="px-2">
+            <Row className="mx-auto justify-content-between">
+              <a href="https://wybc.com/" className="my-auto">
+                <img
+                  src={logo}
+                  height={30}
+                  style={{ display: "block" }}
+                  alt="logo"
+                />
+              </a>
+              <span
+                className={styles.crush + " my-auto"}
+                style={{ fontWeight: "700", fontSize: "28px" }}
+              >
+                C R U S H
+              </span>
               <img
                 src={heart}
-                width={35}
-                height={35}
-                className="mx-auto"
+                width={30}
+                height={30}
+                className="my-auto"
                 alt="heart"
               />
             </Row>
-          </a>
+          </Col>
+        </Row>
+      </div>
+      <Row className={styles.container + " mx-auto"}>
+        <Col xs={1} className="p-0 d-block d-md-none" />
+        <Col
+          xs={1}
+          className={`${styles.title} ${styles.crush} ml-1 p-0 d-none d-md-block`}
+        >
+          <Row className={`mx-auto justify-content-center mt-4 ${styles.logo}`}>
+            <a href="https://wybc.com/">
+              <img
+                src={logo}
+                width={32}
+                className="mx-auto"
+                style={{ display: "block" }}
+                alt="logo"
+              />
+            </a>
+          </Row>
+          <Col className={styles.crush_btn + " px-0"}>
+            <a href="https://wybc-crush.live" style={{ color: "inherit" }}>
+              <Row className="mx-auto justify-content-center">C</Row>
+              <Row className="mx-auto justify-content-center">R</Row>
+              <Row className="mx-auto justify-content-center">U</Row>
+              <Row className="mx-auto justify-content-center">S</Row>
+              <Row className="mx-auto justify-content-center">H</Row>
+
+              <Row
+                className="mx-auto pb-3 justify-content-center"
+                style={{ marginTop: "15px" }}
+              >
+                <img
+                  src={heart}
+                  width={35}
+                  height={35}
+                  className="mx-auto"
+                  alt="heart"
+                />
+              </Row>
+            </a>
+          </Col>
         </Col>
-      </Col>
-      <Col xs={10} className="px-2">
-        <Router>
-          <Switch>
-            <Route exact path="/write" component={Write} />
-            <Route path="/done" component={Done} />
-            <Route path="/letter/:id" component={Letter} />
-            <Route path="/invites/:id" component={Invites} />
-            <Route path="/" component={Landing} />
-          </Switch>
-        </Router>
-      </Col>
-    </Row>
+        <Col xs={10} className="px-2">
+          <Router>
+            <Switch>
+              <Route exact path="/write" component={Write} />
+              <Route path="/done" component={Done} />
+              <Route path="/letter/:id" component={Letter} />
+              <Route path="/invites/:id" component={Invites} />
+              <Route path="/" component={Landing} />
+            </Switch>
+          </Router>
+        </Col>
+      </Row>
+    </>
   );
 }
 
